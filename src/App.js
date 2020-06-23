d83c3826abc99d9c9fceccf95f23e29c2584bec0
jitrogith <coel98er@gmail.com>
2020-06-23 08:21:18 +0000

    ganti

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ class App extends Component {
           <h1 className="App-title">WELCOME MY BRO !</h1>
         </header>
         <p className="App-intro">
-          I was changed on other Branch lately :P
+          GANTIAN YA ...
         </p>
       </div>
     );
